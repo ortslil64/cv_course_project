e4382427bb82c31c19bc9d68421a1aec3ea63d15
ortslil64 <ortslil64@gmail.com>
2016-04-14 08:58:34 +0300

    cv project

---
+++ or.cpp
@@ -0,0 +1,208 @@
+
+//Use image_transport for publishing and subscribing to images in ROS
+#include <image_transport/image_transport.h>
+//Use cv_bridge to convert between ROS and OpenCV Image formats
+#include <cv_bridge/cv_bridge.h>
+//Include some useful constants for image encoding. Refer to: http://www.ros.org/doc/api/sensor_msgs/html/namespacesensor__msgs_1_1image__encodings.html for more info.
+#include <sensor_msgs/image_encodings.h>
+//Include headers for OpenCV Image processing
+#include <opencv2/imgproc/imgproc.hpp>
+//Include headers for OpenCV GUI handling
+#include <opencv2/highgui/highgui.hpp>
+ #include <iostream>
+ #include "opencv2/video/tracking.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+//Store all constants for image encodings in the enc namespace to be used later.
+
+#define MAX_FRAME 1000
+#define MIN_NUM_FEAT 2000
+ using namespace cv;
+using namespace std;
+
+
+void featureTracking(Mat img_1, Mat img_2, vector<Point2f>& points1, vector<Point2f>& points2, vector<uchar>& status)	{ 
+
+//this function automatically gets rid of points for which tracking fails
+
+  vector<float> err;					
+  Size winSize=Size(21,21);																								
+  TermCriteria termcrit=TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 30, 0.01);
+
+  calcOpticalFlowPyrLK(img_1, img_2, points1, points2, status, err, winSize, 3, termcrit, 0, 0.001);
+
+  //getting rid of points for which the KLT tracking failed or those who have gone outside the frame
+  int indexCorrection = 0;
+  for( int i=0; i<status.size(); i++)
+     {  Point2f pt = points2.at(i- indexCorrection);
+     	if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0))	{
+     		  if((pt.x<0)||(pt.y<0))	{
+     		  	status.at(i) = 0;
+     		  }
+     		  points1.erase (points1.begin() + (i - indexCorrection));
+     		  points2.erase (points2.begin() + (i - indexCorrection));
+     		  indexCorrection++;
+     	}
+
+     }
+
+}
+
+
+void featureDetection(Mat img_1, vector<Point2f>& points1)	{   //uses FAST as of now, modify parameters as necessary
+  vector<KeyPoint> keypoints_1;
+  int fast_threshold = 15;
+  bool nonmaxSuppression = true;
+  FAST(img_1, keypoints_1, fast_threshold, nonmaxSuppression);
+  KeyPoint::convert(keypoints_1, points1, vector<int>());
+}
+
+
+int main(int argc, char **argv)
+{
+
+ 
+int iii=0;
+
+
+    VideoCapture cap("/home/or/opencv_ws/src/cv_or/or.avi"); //capture the video from web cam
+    
+sleep(3);
+    if ( !cap.isOpened() )  // if not success, exit program
+    {
+         cout << "Cannot open the web cam" << endl;
+         return -1;
+    }
+int numFrame=2;
+  Mat img_1, img_2;
+  Mat R_f, t_f; //the final rotation and tranlation vectors containing the 
+  Mat img_1_c,img_2_c;
+ cap.read(img_1_c); 
+    cap.read(img_2_c);  
+          cvtColor(img_1_c, img_1, COLOR_BGR2GRAY);
+  cvtColor(img_2_c, img_2, COLOR_BGR2GRAY);
+  double fontScale = 1;
+  double scale = 1.00;
+  vector<Point2f> points1, points2;        //vectors to store the coordinates of the feature points
+  featureDetection(img_1, points1);        //detect features in img_1
+  vector<uchar> status;
+  featureTracking(img_1,img_2,points1,points2, status); //track those features to img_2
+
+ double focal = 300.8560;
+  cv::Point2d pp(607.1928, 185.2157);
+  //recovering the pose and the essential matrix
+  Mat E, R, t, mask;
+  E = findEssentialMat(points2, points1, focal, pp, RANSAC, 0.999, 1.0, mask);
+  recoverPose(E, points2, points1, R, t, focal, pp, mask);
+
+  Mat prevImage = img_2;
+  Mat currImage;
+  vector<Point2f> prevFeatures = points2;
+  vector<Point2f> currFeatures;
+
+  R_f = R.clone();
+  t_f = t.clone();
+
+  clock_t begin = clock();
+
+  namedWindow( "Road facing camera", WINDOW_AUTOSIZE );// Create a window for display.
+  namedWindow( "Trajectory", WINDOW_AUTOSIZE );// Create a window for display.
+
+  Mat traj = Mat::zeros(600, 600, CV_8UC3);
+  Mat currImage_c;
+    while (true)
+    {
+   cap.read(currImage_c); 
+cvtColor(currImage_c, currImage, COLOR_BGR2GRAY);
+  	vector<uchar> status;
+  	featureTracking(prevImage, currImage, prevFeatures, currFeatures, status);
+  	
+  	
+  	
+  	  	E = findEssentialMat(currFeatures, prevFeatures, focal, pp, RANSAC, 0.999, 1.0, mask);
+  	recoverPose(E, currFeatures, prevFeatures, R, t, focal, pp, mask);
+
+    Mat prevPts(2,prevFeatures.size(), CV_64F), currPts(2,currFeatures.size(), CV_64F);
+
+
+   for(int i=0;i<prevFeatures.size();i++)	{   //this (x,y) combination makes sense as observed from the source code of triangulatePoints on GitHub
+  		prevPts.at<double>(0,i) = prevFeatures.at(i).x;
+  		prevPts.at<double>(1,i) = prevFeatures.at(i).y;
+
+  		currPts.at<double>(0,i) = currFeatures.at(i).x;
+  		currPts.at<double>(1,i) = currFeatures.at(i).y;
+    }
+    
+    
+    
+    
+    
+    
+    
+
+    
+
+    if ((scale>0.1)&&(t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))) {
+
+      t_f = t_f + scale*(R_f*t);
+      R_f = R*R_f;
+
+    }
+  	
+    else {
+     //cout << "scale below 0.1, or incorrect translation" << endl;
+    }
+    
+   // lines for printing results
+   // myfile << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2) << endl;
+
+  // a redetection is triggered in case the number of feautres being trakced go below a particular thresho
+    
+    
+ 	  if (prevFeatures.size() < MIN_NUM_FEAT)	{
+      //cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
+      //cout << "trigerring redection" << endl;
+ 		  featureDetection(prevImage, prevFeatures);
+      featureTracking(prevImage,currImage,prevFeatures,currFeatures, status);
+
+ 	  }
+
+    prevImage = currImage.clone();
+    prevFeatures = currFeatures;
+
+    int x = int(t_f.at<double>(0)) + 300;
+    int y = int(t_f.at<double>(2)) + 100;
+    circle(traj, Point(x, y) ,1, CV_RGB(255,0,0), 2);
+
+    rectangle( traj, Point(10, 30), Point(550, 50), CV_RGB(0,0,0), CV_FILLED);
+    
+    for(iii=0;iii<currFeatures.size();iii++){
+		
+		circle( currImage_c, Point( currFeatures[iii].x, currFeatures[iii].y ), 5,  Scalar(200), 2, 1, 0 );
+		
+	}
+    
+    
+    imshow( "Road facing camera", currImage_c );
+    imshow( "Trajectory", traj );
+    
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+       {
+            cout << "esc key is pressed by user" << endl;
+            break; 
+       }
+    numFrame++;
+    }
+/////////////////////
+  
+     
+    
+    
+        
+
+ 
+
+
+}
